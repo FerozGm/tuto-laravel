ec51af90a3d2bcc84ebb2b8e990f0f9d359e07d6
Frédéric <frederic.noble5@etu.univ-lorraine.fr>
2020-09-21 20:35:49 +0200

    Correction de l'edition des contacts

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -4,7 +4,7 @@
 
 <h1>Créer un nouveau client</h1>
 <form action="/clients" method="POST">
-    @method('PATCH')
+    @method('POST')
     @include('includes.form')
     <button type="submit" class="btn btn-primary">Ajouter le client</button>
 </form>
